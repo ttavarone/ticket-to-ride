a8a244446c96caed249097d3cf318f7f371450bf
lj10bran <lj10bran@siena.edu>
2018-04-12 13:44:23 -0400

    Put the images from the blackboard site into the github folder

---
+++ TrainCard.java
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * Write a description of class TrainCard here.
+ * 
+ * @author Logan Brandt, Tucker Tavarone, Thomas Fresenius, 
+ * Eamonn Conway, Josh DelSignore 
+ * @version 1.0
+ */
+public class TrainCard
+{
+    protected String cardType = "";
+    
+    /**
+     * This method initializes the TrainCard with the type of
+     * train the card has.
+     * @param newCardType - The type of train portrayed on the card
+     */
+    TrainCard(String newCardType)
+    {
+        cardType = newCardType;
+    }
+}
