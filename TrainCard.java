a8265424a03266c666d3f1079e7424a605f3a962
lj10bran <lj10bran@siena.edu>
2018-04-12 14:16:45 -0400

    Added in the cards

--- TrainCard.java
+++ TrainCard.java
@@ -1,22 +1,46 @@
 ////////////////////////////////////////////////////////////////////////////////
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 /**
- * Write a description of class TrainCard here.
+ * This method creates a single instance of one of the train cards a
+ * player would draw.
  * 
- * @author Logan Brandt, Tucker Tavarone, Thomas Fresenius, 
+ * @author Logan Brandt, Tucker Tavarone, Tom Fresenius, 
  * Eamonn Conway, Josh DelSignore 
  * @version 1.0
  */
 public class TrainCard
 {
-    protected String cardType = "";
-    
+    protected int cardNum;
+    protected Toolkit toolkit;
+    private Image[] imgArr;
+    private Image blackTrain = toolkit.getImage("TicketToRidePics\\BlackCard");
+    private Image blueTrain = toolkit.getImage("TicketToRidePics\\BlueCard");
+    private Image greenTrain = toolkit.getImage("TicketToRidePics\\GreenCard");
+    private Image orangeTrain = toolkit.getImage("TicketToRidePics\\OrangeCard");
+    private Image purpleTrain = toolkit.getImage("TicketToRidePics\\PurpleCard");
+    private Image redTrain = toolkit.getImage("TicketToRidePics\\RedCard");
+    private Image whiteTrain = toolkit.getImage("TicketToRidePics\\WhiteCard");
+    private Image yellowTrain = toolkit.getImage("TicketToRidePics\\YellowCard");
+    private Image wildCard = toolkit.getImage("TicketToRidePics\\RainbowCard");
+
     /**
      * This method initializes the TrainCard with the type of
      * train the card has.
      * @param newCardType - The type of train portrayed on the card
      */
-    TrainCard(String newCardType)
+    TrainCard(int newCardNum)
     {
-        cardType = newCardType;
+        cardNum = newCardNum;
+        
+        imgArr = new Image[]{blackTrain, blueTrain, greenTrain, orangeTrain, purpleTrain,
+        redTrain, whiteTrain, yellowTrain, wildCard};
+        
+    }
+    
+    public Image getTrainCard()
+    {
+        return imgArr[cardNum];
     }
 }
